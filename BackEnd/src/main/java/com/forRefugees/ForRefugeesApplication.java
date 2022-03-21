80168e21e67a35ccc1b2f4f414053a985c67c1ee
Janilson Coimbra <janilson.coimbra@gmail.com>
2022-03-21 13:24:12 -0300

    Chore Init project backEnd

---
+++ BackEnd/src/main/java/com/forRefugees/ForRefugeesApplication.java
@@ -0,0 +1,13 @@
+package com.forRefugees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForRefugeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForRefugeesApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/forRefugees/ForRefugeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forRefugees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForRefugeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
