65c9fb4da63cc9672d7026dd710fdf0a58715db3
Janilson Coimbra <janilson.coimbra@gmail.com>
2022-03-22 01:49:20 -0300

    Chore implements CrossOrigin

--- BackEnd/src/main/java/com/forRefugees/controller/RefugiadosController.java
+++ BackEnd/src/main/java/com/forRefugees/controller/RefugiadosController.java
@@ -3,6 +3,7 @@ package com.forRefugees.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.forRefugees.services.RefugiadosService;
 
 @RestController
 @RequestMapping("/refugiados")
+@CrossOrigin(origins = "*")
 public class RefugiadosController {
 
 	@Autowired
