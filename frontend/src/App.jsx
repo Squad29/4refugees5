d355954bfcefb51cd39f15783f60611e42babf04
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-03-22 11:27:22 -0300

    Initiated Home Page and install react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import NavbarGlobal from "./components/NavbarGlobal";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <NavbarGlobal />
+    <BrowserRouter>
+      <NavbarGlobal />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/destinos" element={<Destinos />} />
+        <Route path="/hoteis" element={<Hoteis />} />
+        <Route path="/pacotes" element={<Pacotes />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/destinos" element={<AdmDestino />} />
+        <Route path="/admin/hoteis" element={<AdmHotel />} /> */}
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
