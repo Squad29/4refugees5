de1fdcb2f798498f1a3863ba10f3282d7230e823
Rafael Moura <rafaelfelipe.peixoto@gmail.com>
2022-03-22 12:59:44 -0300

    Insert DATA AOS

--- frontend/src/components/NavbarGlobal/index.jsx
+++ frontend/src/components/NavbarGlobal/index.jsx
@@ -1,54 +1,63 @@
 import React from "react";
 
+import './styles.css'
+
 function NavbarGlobal() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/" rel="noreferrer">
-          4Refugees
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/" rel="noreferrer">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/" rel="noreferrer">
-                Refugiado
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/" rel="noreferrer">
-                Contratante
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/" rel="noreferrer">
-                Vagas
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/" rel="noreferrer">
-                Contato
-              </a>
-            </li>
-          </ul>
+    <header id="navbarGlobal">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/" rel="noreferrer">
+            4Refugees
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/"
+                  rel="noreferrer"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/" rel="noreferrer">
+                  Refugiado
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/" rel="noreferrer">
+                  Contratante
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/" rel="noreferrer">
+                  Vagas
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/" rel="noreferrer">
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import videoHero from "../../assets/video/heroVideo.mp4";
 
+import './styles.css'
 
 function Home() {
   return (
-    <h1>Página Home</h1>
+    <main>
+      <section id="hero--section">
+        <video autoPlay muted loop id="myVideo">
+          <source src={videoHero} type="video/mp4" />
+        </video>
+        <div id="slogan" data-aos="fade-up" data-aos-duration="3000">
+          <p>
+            For Refugees
+          </p>
+          <p>
+            "Juntos vamos mais longe!"
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
 
